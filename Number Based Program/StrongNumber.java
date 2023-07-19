daf8e6f24cb5162775f6031da208f1d87b9b07a2
KhemrajNB <khemrajbohra58@gmail.com>
2023-07-19 07:08:34 +0530

    StromgNumber

---
+++ Number Based Program/StrongNumber.java
@@ -0,0 +1,22 @@
+public class StrongNumber {	
+	public static boolean isStrong(int n) {
+		int temp = n;
+		int sum = 0;
+		while(temp>0) {
+			int fact = 1;
+			int num = temp%10;
+			for(;num>0;num--) {
+				fact *= num;
+			}
+			sum += fact;
+			temp = temp/10;
+		}
+		
+		return n==sum;
+	}
+	public static void main(String[] args) {
+		System.out.println(isStrong(145));
+
+	}
+
+}
